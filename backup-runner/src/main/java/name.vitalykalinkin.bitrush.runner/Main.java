3f60741f887d4f0a268fcef746416910b74d946e
Vitaly Kalinkin <vitaly.kalinkin@gmail.com>
2014-03-13 08:36:41 +0400

    Added logging via commons-logging and log4j
    Added AWS SDK

--- backup-runner/src/main/java/name.vitalykalinkin.bitrush.runner/Main.java
+++ backup-runner/src/main/java/name.vitalykalinkin.bitrush.runner/Main.java
@@ -1,8 +1,12 @@
 package name.vitalykalinkin.bitrush.runner;
 
 import org.apache.commons.cli.*;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class Main {
+    private final static Log log = LogFactory.getLog(Main.class);
+
     public static void main(String args[]) throws ParseException {
         Options options = OptionsDescription.getOptions();
 
@@ -10,7 +14,8 @@ public class Main {
         CommandLine cmd = parser.parse(options, args);
 
         if (cmd.hasOption("t")) {
-            System.out.println("AAAAAAA");
+            log.info("AAAAAAA");
+            log.trace("BBBBBBBBB");
         }
 
         HelpFormatter formatter = new HelpFormatter();
