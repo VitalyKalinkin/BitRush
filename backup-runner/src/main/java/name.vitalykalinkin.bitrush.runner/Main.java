5f199be1c5b1967719253476ff4dbfba4f197944
Vitaly Kalinkin <vitaly.kalinkin@gmail.com>
2014-03-19 22:34:22 +0400

    Moved to JDK 1.8
    Added stream trivial test.

--- backup-runner/src/main/java/name.vitalykalinkin.bitrush.runner/Main.java
+++ backup-runner/src/main/java/name.vitalykalinkin.bitrush.runner/Main.java
@@ -4,6 +4,9 @@ import org.apache.commons.cli.*;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     private final static Log log = LogFactory.getLog(Main.class);
 
@@ -12,14 +15,18 @@ public class Main {
 
         CommandLineParser parser = new GnuParser();
 
+        Stream<String> testStream = Stream.builder().add("Test").add("Another").build().map(x -> (String) x);
+        String str = testStream.collect(Collectors.joining("}|{"));
+
+        log.warn("Stream processing result: " + str);
+
         try {
             CommandLine cmd = parser.parse(options, args);
 
-            if (cmd.hasOption("c")) {
-                log.info(String.format("Configuration file: %s", cmd.getOptionValue("c", "megaconfig.conf")));
-                log.trace("BBBBBBBBB");
-            }
+            log.info(String.format("Configuration file: %s", cmd.getOptionValue("c", "megaconfig.conf")));
+            log.trace("BBBBBBBBB");
         } catch (ParseException ex) {
+            log.fatal(String.format("Could not parse command line options: %s", ex.getMessage()));
             HelpFormatter formatter = new HelpFormatter();
             formatter.printHelp("backup-runner", options);
             return;
