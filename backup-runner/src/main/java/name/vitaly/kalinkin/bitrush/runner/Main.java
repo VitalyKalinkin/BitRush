1474ea0ed4aee97fa6e1f18781053221e966ed03
Vitaly Kalinkin <vitaly.kalinkin@gmail.com>
2014-04-16 13:39:57 +0400

    Corrected folder names for the main package in the runner.

--- backup-runner/src/main/java/name/vitaly/kalinkin/bitrush/runner/Main.java
+++ backup-runner/src/main/java/name/vitaly/kalinkin/bitrush/runner/Main.java
@@ -8,22 +8,22 @@ public class Main {
     private final static Log log = LogFactory.getLog(Main.class);
 
     public static void main(String args[]){
-        name.vitaly.kalinkin.bitrush.runner.CommandLineOptions configOptions = parseCommandLine(args);
+        CommandLineOptions configOptions = parseCommandLine(args);
 
         if (configOptions.isEmpty())
             return;
     }
 
     private static name.vitaly.kalinkin.bitrush.runner.CommandLineOptions parseCommandLine(String[] args) {
-                                            ≈
+
         Options options = name.vitaly.kalinkin.bitrush.runner.OptionDescriptionFactory.getOptions();
 
         CommandLineParser parser = new GnuParser();
 
         try {
             CommandLine cmd = parser.parse(options, args);
-            name.vitaly.kalinkin.bitrush.runner.CommandLineOptions result =
-                    new name.vitaly.kalinkin.bitrush.runner.CommandLineOptions(cmd.getOptions());
+            CommandLineOptions result =
+                    new CommandLineOptions(cmd.getOptions());
 
         } catch (ParseException ex) {
             log.fatal(String.format("Could not parse command line options: %s", ex.getMessage()));
